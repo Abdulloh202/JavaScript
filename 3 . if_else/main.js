95654632fc12d02317d25693e1a769b7591a6cb3
abdulloh <dragonm7274gmail.com>
2023-08-22 19:57:06 +0500

    If else

---
+++ 3 . if_else/main.js
@@ -0,0 +1,41 @@
+// HomeWork
+// 1
+// let a = 1;
+// let b = 2;
+
+// if (a + b < 4) {
+//   console.log("Bellow");
+// } else {
+//   console.log("Over");
+// }
+
+// 2
+// let login = "employed";
+
+// if (login == "employed") {
+//   console.log("Hello");
+// } else if (login == "director") {
+//   console.log("greetings");
+// } else if (login == "") {
+//   console.log("no login");
+// } else {
+//   console.log("It is not key word");
+// }
+
+// 3 switch case
+
+let browser = '';
+
+switch (browser) {
+  case "Edge":
+    console.log("You've got the Edge");
+    break;
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    console.log("Okay we support these browsers too");
+    break;
+    default:
+        console.log('We hope that this page looks ok!');
+}
