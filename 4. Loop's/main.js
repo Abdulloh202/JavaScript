f770b0bd2fb8e5592df8fd45f7ce684069714271
abdulloh <dragonm7274gmail.com>
2023-08-25 12:46:35 +0500

    Loop's

--- 4. Loop's/main.js
+++ 4. Loop's/main.js
@@ -19,7 +19,6 @@
 //   str = "";
 // }
 
-
 // 2
 // let width = 10;
 // let height = 5;
@@ -50,8 +49,8 @@
 //       str+="*"
 //     }else if(i+j == width -1){
 //       str+="*"
-//     }else if(i ==j){
-//       str+="1"
+//     }else if(i==j){
+//       str+="*"
 //     }
 //      else{
 //       str +=" "
@@ -81,4 +80,72 @@
 //   str += "\n";
 // }
 
-//console.log(str);
+// console.log(str);
+
+
+// qudarat aka
+
+// 1
+// let n = 2,
+//   k = 3;
+
+//   for(let i = 1;i<=n;i++){
+//     console.log(k);
+//   }
+
+// 2
+// let a = 1;
+// let b = 10;
+// let s = 0;
+// for (let i = a; i <= b; i++) {
+//     s++
+//   console.log(i);
+// }
+// console.log(`${s} ta son chiqarildi`);
+
+
+// 3
+// let a = 1;
+// let b = 10;
+// let s = 0;
+// for (let i = b -1; i > a; i--) {
+//     s++
+//   console.log(i);
+// }
+// console.log(`${s} ta son chiqarildi`);
+
+
+// 4
+
+// let kg = 1;
+// let narx = "5000"
+// for(let i = 1;i<=kg;i++){
+//     console.log(`${i}kg`);
+// }
+
+
+
+// let width = 11;
+// let height = 11;
+// let str=""
+// for (let i = 0; i < height; i++) {
+
+//   for (let j = 0; j < width; j++) {
+//     if (j == 0 || j == width -1) {
+//       str+="*"
+//     }else if(i== -1 || i==height-1){
+//       str+="*"
+//     }else if(i+j == width -1){
+//       str+="*"
+//     }else if(i==j){
+//       str+="*"
+//     }else if(j=== width){
+//         str+="*"
+//     }
+//      else{
+//       str +=" "
+//      }
+//   }
+//   str += "\n"
+// }
+// console.log(str);
